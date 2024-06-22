8cfe0c4230467ec91e486a20c1362afaf79dd582
Ákos <113618285+akoos1204@users.noreply.github.com>
2024-06-22 15:52:29 +0200

    Updated ping command
    More advanceable ping command, now you can refresh it.

--- src/commands/Community/ping.js
+++ src/commands/Community/ping.js
@@ -1,10 +1,56 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction, client) {
-		await interaction.reply({content: `Pong! The API Latency is: ${interaction.client.ws.ping}ms.`, ephemeral: true});
-	},
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Displays the bot\'s latency with a refresh button'),
+
+  async execute(interaction) {
+    const ping = Date.now() - interaction.createdTimestamp;
+    const apiPing = Math.round(interaction.client.ws.ping);
+
+    const embed = new EmbedBuilder()
+      .setColor('DarkGrey')
+      .setTitle(':ping_pong: Pong!')
+      .addFields(
+        { name: 'Bot Latency', value: `${ping}ms`, inline: true },
+        { name: 'API Latency', value: `${apiPing}ms`, inline: true }
+      );
+
+    const refreshButton = new ButtonBuilder()
+      .setCustomId('refresh_ping')
+      .setLabel('Refresh')
+      .setStyle(ButtonStyle.Primary);
+
+    const row = new ActionRowBuilder()
+    .addComponents(refreshButton);
+
+    await interaction.reply({ embeds: [embed], components: [row], ephemeral: true });
+
+    const filter = i => i.customId === 'refresh_ping' && i.user.id === interaction.user.id;
+    const collector = interaction.channel.createMessageComponentCollector({ filter });
+
+    collector.on('collect', async i => {
+      if (i.customId === 'refresh_ping') {
+        const newPing = Date.now() - i.createdTimestamp;
+        const newApiPing = Math.round(interaction.client.ws.ping);
+
+        const newEmbed = new EmbedBuilder()
+          .setColor('DarkGrey')
+          .setTitle(':ping_pong: Pong!')
+          .addFields(
+            { name: 'Bot Latency', value: `${newPing}ms`, inline: true },
+            { name: 'API Latency', value: `${newApiPing}ms`, inline: true }
+          );
+
+        await i.update({ embeds: [newEmbed], components: [row]});
+      }
+    });
+
+    collector.on('end', collected => {
+      if (collected.size === 0) {
+        interaction.editReply({ components: [], ephemeral: true });
+      }
+    });
+  }
 };
